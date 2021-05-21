b251c96a54778b1e1190153f91236b6e3fac0514
Zyon Xiong <xiong.houa95@gmail.com>
2021-05-21 03:48:18 +0000

    was able to add new student, create a link to see page, but unable to create a display page yet. Also, remember to pg_dump and check db into HB.sql

--- hackbright_web.py
+++ hackbright_web.py
@@ -23,11 +23,17 @@ def get_student():
 
     return html
 
-@app.route('/student-search')
+@app.route('/student-search') #go here first otherwise nothing will be saved and run in '/student'
 def get_student_form():
     """Show form for searching for a student"""
 
     return render_template("student_search.html")
+
+@app.route('/student-display-form')
+def student_form():
+     """this is front desk that displays the blank add a new student form """
+     return render_template("student_add.html")
+
 
 @app.route('/student-add', methods=["POST"])
 def student_add():
@@ -41,21 +47,23 @@ def student_add():
     #new_student = request.form['github']
     #alternative
 
-    fname, lname, github = hackbright.make_new_student(fname, lname, github)
+    hackbright.make_new_student(fname, lname, github)
 
-    html = render_template("student_add.html",
+    html = render_template("student_confirmation.html",
                             fname=fname,
                             lname=lname,
                             github=github)
 
-    return html
+    return (html)
+
 
 @app.route('/new-student-display')
 def student_display():
-    pass 
+
+    #return render_template(some_page.htmls)
+    return render_template('student_confirmation.html')
 
 
-    
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
